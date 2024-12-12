2abd1bc0853bed0e818a415e80000b946f575f5e
Kerim Akgun <10kerimakgun28@gmail.com>
2024-12-12 23:37:24 +0300

    first

---
+++ ags/src/lib/config.js
@@ -0,0 +1,36 @@
+const Notification = await Service.import("notifications")
+Notification.popupTimeout = 10000
+Notification.forceTimeout = false
+
+export default {
+    workspaceCount: 10,
+    maxPopups: 5,
+    powerMenu: ["lock", "logout", "restart", "shutdown"],
+
+    animation: {
+        baseAnimation: 500,
+        player: 500,
+        systemTray: 500,
+        widgets: 500,
+        popup: {
+            actions: 200,
+            down: 150,
+            left: 200,
+        },
+    },
+
+    commands: {
+        clock: "date '+%a, %d %b %I:%M %p'",
+        colorPicker: "sh -c 'hyprpicker | wl-copy'",
+        inhibit: {
+            on: "wlinhibit",
+            off: "pkill wlinhibit",
+        },
+        power: {
+            shutdown: "systemctl poweroff",
+            restart: "systemctl reboot",
+            logout: "hyprctl dispatch exit",
+            lock: "hyprlock",
+        },
+    },
+}
